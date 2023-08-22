ce84d74a607f9a7c999eaf10801674323e5ddfdb
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-08-22 14:08:57 +0900

    design/#3: 디자인 시스템 추가 (#4)

---
+++ fe/src/styles/base/Colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  white: '#FFFFFF',
+  gray50: '#FAFAFA',
+  gray100: 'rgba(249, 249, 249, 0.8)',
+  gray200: 'rgba(245, 245, 245, 0.7)',
+  gray300: 'rgba(179, 179, 179, 0.12)',
+  gray400: '#EFEFF0',
+  gray500: 'rgba(179, 179, 179, 0.39)',
+  gray600: 'rgba(0, 0, 0, 0.2)',
+  gray700: 'rgba(60, 60, 67, 0.36)',
+  gray800: 'rgba(60, 60, 67, 0.6)',
+  gray900: '#3C3C43',
+  black: '#000000',
+  mint: '#00C7BE',
+  orange: '#FF9500',
+  red: '#FF3B30',
+};
