53005ae1159c6bc7c3c0cbcdac33eee8128efbb7
WK <mwk0725@gmail.com>
2023-08-24 11:35:39 +0900

    refactor/#6: 디자인 시스템 수정

--- fe/src/styles/DesignSystem.ts
+++ fe/src/styles/DesignSystem.ts
@@ -1,57 +1,45 @@
 import { colors } from '@styles/base/Colors';
 import { fonts } from '@styles/base/Fonts';
 
-export const lightMode = {
+export const theme = {
   color: {
-    neutral: {
-      text: {
-        default: colors.gray900,
-        weak: colors.gray800,
-        strong: colors.black,
-      },
-      background: {
-        default: colors.white,
-        weak: colors.gray50,
-        bold: colors.gray400,
-        blur: colors.gray100,
-      },
-      border: {
-        default: colors.gray500,
-        strong: colors.gray700,
-      },
-      overay: {
-        default: colors.gray600,
-      },
-    },
-    accent: {
-      textWeak: colors.black,
-      primary: colors.orange,
-      secondary: colors.mint,
-    },
-    system: {
-      warning: colors.red,
-      background: colors.white,
-      backgroundWeak: colors.gray200,
-    },
+    neutralText: colors.gray900,
+    neutralTextWeak: colors.gray800,
+    neutralTextStrong: colors.black,
+
+    neutralBackground: colors.white,
+    neutralBackgroundWeak: colors.gray50,
+    neutralBackgroundBold: colors.gray400,
+    neutralBackgroundBlur: colors.gray100,
+
+    neutralborder: colors.gray500,
+    neutralborderStrong: colors.gray700,
+
+    neutralOveray: colors.gray600,
+
+    accentText: colors.white,
+    accentTextWeak: colors.black,
+    accentPrimary: colors.orange,
+    accentSecondary: colors.mint,
+
+    systemWarning: colors.red,
+    systemBackground: colors.white,
+    systemBackgroundWeak: colors.gray200,
   },
 
   font: {
-    display: {
-      strong20: fonts.bold20,
-      strong16: fonts.bold16,
-      default16: fonts.regular16,
-      default12: fonts.regular12,
-    },
-    available: {
-      strong16: fonts.bold16,
-      strong12: fonts.bold12,
-      strong10: fonts.bold10,
-      default16: fonts.regular16,
-      default12: fonts.regular12,
-    },
-    enabled: {
-      strong16: fonts.bold16,
-      strong10: fonts.bold10,
-    },
+    displayStrong20: fonts.bold20,
+    displayStrong16: fonts.bold16,
+    displayDefault16: fonts.regular16,
+    displayDefault12: fonts.regular12,
+
+    availableStrong16: fonts.bold16,
+    availableStrong12: fonts.bold12,
+    availableStrong10: fonts.bold10,
+    availableDefault16: fonts.regular16,
+    availableDefault12: fonts.regular12,
+
+    enabledStrong16: fonts.bold16,
+    enabledStrong10: fonts.bold10,
   },
 };
