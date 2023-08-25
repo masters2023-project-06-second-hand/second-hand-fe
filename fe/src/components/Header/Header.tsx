ef7968ee7e033b9ab14ac3e5890af337efdfdf1e
ᴡᴏɴᴋʏᴜɴɢ ᴍɪɴ <mwk0725@gmail.com>
2023-08-25 09:40:00 +0900

    feat/#10: Header 컴포넌트 구현 (#13)

---
+++ fe/src/components/Header/Header.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+interface HeaderProps {
+  backgroundColor?: string;
+  children?: React.ReactNode;
+}
+
+const LeftContent = styled.div``;
+const CenterContent = styled.h2`
+  flex: 1;
+  text-align: center;
+  font: ${({ theme: { font } }) => font.displayStrong16};
+  color: ${({ theme: { color } }) => color.neutralTextStrong};
+`;
+const RightContent = styled.div``;
+
+export const Header: React.FC<HeaderProps> & {
+  Left: typeof LeftContent;
+  Center: typeof CenterContent;
+  Right: typeof RightContent;
+} = ({ children, backgroundColor = 'neutralBackgroundBlur' }) => {
+  return (
+    <HeaderContainer $backgroundColor={backgroundColor}>
+      {children}
+    </HeaderContainer>
+  );
+};
+
+Header.Left = LeftContent;
+Header.Center = CenterContent;
+Header.Right = RightContent;
+
+const HeaderContainer = styled.header<{ $backgroundColor: string }>`
+  width: 100%;
+  max-width: 393px;
+  width: 393px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px;
+  height: 56px;
+  border-bottom: 1px solid ${({ theme: { color } }) => color.neutralborder};
+  background-color: ${({ theme, $backgroundColor = 'neutralBackgroundBlur' }) =>
+    theme.color[$backgroundColor]};
+`;
