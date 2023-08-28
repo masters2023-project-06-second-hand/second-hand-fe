12034952d913069f217bf1e40be64dc98495eac4
ᴡᴏɴᴋʏᴜɴɢ ᴍɪɴ <mwk0725@gmail.com>
2023-08-28 17:18:57 +0900

    feat/#16: FabButton 컴포넌트 구현 (#25)

---
+++ fe/src/components/Button/FabButton.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { Icon } from '@components/Icon/Icon';
+
+export type ButtonProps = {
+  onClick: () => void;
+};
+
+export const FabButton: React.FC<ButtonProps> = () => {
+  return (
+    <Fab>
+      <Icon name="plus" stroke="accentText" size="M" />
+    </Fab>
+  );
+};
+
+const Fab = styled.button`
+  width: 56px;
+  height: 56px;
+  background-color: ${({ theme: { color } }) => color.accentPrimary};
+  border-radius: ${({ theme: { radius } }) => radius.half};
+`;
