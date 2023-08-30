2b117bf0d1133c733d9db4813573c4ca555b4f85
qkdflrgs <qkdflrgs@gmail.com>
2023-08-30 19:20:50 +0900

    feat/#43: PageNav 컴포넌트 구현

---
+++ fe/src/components/PageNav/PageNav.tsx
@@ -0,0 +1,33 @@
+import { styled } from 'styled-components';
+
+type Props = {
+  curPage: number;
+  numberOfPage: number;
+};
+
+export const PageNav: React.FC<Props> = ({ curPage, numberOfPage }) => {
+  return (
+    <Wrapper>
+      <PageInfo>
+        {curPage} / {numberOfPage}
+      </PageInfo>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  padding: 8px 16px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: fit-content;
+  height: 32px;
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.color.neutralBackgroundBlur};
+  user-select: none;
+`;
+
+const PageInfo = styled.span`
+  font: ${({ theme }) => theme.font.displayDefault12};
+  color: ${({ theme }) => theme.color.neutralTextWeak};
+`;
