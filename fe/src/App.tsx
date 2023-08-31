925e7f4234e8b488edbcb098615e52287e83cc95
qkdflrgs <qkdflrgs@gmail.com>
2023-09-01 02:33:30 +0900

    refactor/#61: App에 AddPage 컴포넌트 추가

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { theme } from '@styles/DesignSystem';
 import { ThemeProvider } from 'styled-components';
+import { AddPage } from '@pages/AddPage';
 
 const queryClient = new QueryClient({
   /* options */
@@ -12,7 +13,9 @@ function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
-        <ThemeProvider theme={theme}></ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <AddPage />
+        </ThemeProvider>
       </QueryClientProvider>
     </>
   );
