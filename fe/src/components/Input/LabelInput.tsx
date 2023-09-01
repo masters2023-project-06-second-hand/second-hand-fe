ec8df0f09870819d79331cb087032bbe1dcc8393
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-09-01 11:13:09 +0900

    feat/#31: Input 컴포넌트 구현 (#48)

---
+++ fe/src/components/Input/LabelInput.tsx
@@ -0,0 +1,44 @@
+import { styled } from 'styled-components';
+import React from 'react';
+import { InputBox } from './InputBox';
+
+type Props = {
+  id: string;
+  label: string;
+  placeholder?: string;
+  value: string;
+  onChange(e: React.ChangeEvent<HTMLInputElement>): void;
+};
+
+export const LabelInput: React.FC<Props> = ({
+  id,
+  label,
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return (
+    <Wrapper>
+      <Label htmlFor={id}>{label}</Label>
+      <InputBox
+        type="rect"
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+`;
+
+const Label = styled.label`
+  font: ${({ theme }) => theme.font.displayStrong16};
+  color: ${({ theme }) => theme.color.neutralTextStrong};
+`;
