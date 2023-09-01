d3c12c79895b3be4d0c28e3308cfe2e16d096e84
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-09-01 11:13:46 +0900

    feat/#30: Textarea 컴포넌트 구현 (#49)

---
+++ fe/src/components/Textarea/Textarea.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef } from 'react';
+import { styled } from 'styled-components';
+
+type Props = {
+  placeholder?: string;
+  defaultRows?: number;
+  value: string;
+  onChange(e: React.ChangeEvent<HTMLTextAreaElement>): void;
+};
+
+export const Textarea: React.FC<Props> = ({
+  placeholder,
+  defaultRows = 2,
+  value,
+  onChange,
+}) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleResizeHeight = () => {
+    const { current } = textareaRef;
+    if (current) {
+      current.style.height = 'auto';
+      current.style.height = current.scrollHeight + 'px';
+    }
+  };
+
+  useEffect(() => {
+    handleResizeHeight();
+  }, [value]);
+
+  return (
+    <StyledTextarea
+      ref={textareaRef}
+      rows={defaultRows}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+    ></StyledTextarea>
+  );
+};
+
+const StyledTextarea = styled.textarea`
+  padding: 0;
+  width: 100%;
+  font: ${({ theme }) => theme.font.availableDefault16};
+  color: ${({ theme }) => theme.color.neutralTextStrong};
+  resize: none;
+  border: none;
+  outline: none;
+  &::placeholder {
+    color: ${({ theme }) => theme.color.neutralTextWeak};
+  }
+`;
