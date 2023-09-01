57499e2cfa8f01bd4c81f01dbb4af4a2f6371b11
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-09-01 11:13:09 +0900

    feat/#31: Input 컴포넌트 구현 (#48)

---
+++ fe/src/components/Input/Input.tsx
@@ -0,0 +1,35 @@
+import React, { InputHTMLAttributes } from 'react';
+import { styled } from 'styled-components';
+
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  value: string;
+  onChange(e: React.ChangeEvent<HTMLInputElement>): void;
+};
+
+export const Input: React.FC<Props> = ({
+  type = 'text',
+  placeholder = '내용을 입력하세요',
+  value,
+  onChange,
+}) => {
+  return (
+    <StyledInput
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+const StyledInput = styled.input`
+  padding: 0px;
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  font: ${({ theme }) => theme.font.availableDefault16};
+  color: ${({ theme }) => theme.color.neutralTextStrong};
+  caret-color: #007aff;
+`;
