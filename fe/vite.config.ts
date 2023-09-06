f20f12e7f11f7f5fd665b57dd0fb5c40ecfba61e
ᴡᴏɴᴋʏᴜɴɢ ᴍɪɴ <mwk0725@gmail.com>
2023-09-06 11:17:29 +0900

    feat/#76: axios 설정, 환경 변수 설정 (#77)

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       '@constants': resolve(__dirname, './src/constants'),
       '@test': resolve(__dirname, './src/test'),
       '@routes': resolve(__dirname, './src/routes'),
+      '@api': resolve(__dirname, './src/api'),
     },
   },
 });
