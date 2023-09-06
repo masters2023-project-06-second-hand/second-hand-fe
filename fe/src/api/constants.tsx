e95ed5d99482a3cacf5cc3bfc7a0f6266456b7e2
ᴡᴏɴᴋʏᴜɴɢ ᴍɪɴ <mwk0725@gmail.com>
2023-09-06 11:45:35 +0900

    feat/#78: category 목록 관련 요청 추가 (#79)

--- fe/src/api/constants.tsx
+++ fe/src/api/constants.tsx
@@ -6,3 +6,7 @@ export const BASE_API_URL =
 
 export const GOOGLE_OAUTH_URL = VITE_APP_GOOGLE_OAUTH_URL;
 export const NAVER_OAUTH_URL = VITE_APP_NAVER_OAUTH_URL;
+
+export const API_ENDPOINTS = {
+  CATEGORIES: '/categories',
+};
