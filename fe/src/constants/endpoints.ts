b41ed855e2b2bda2fb8f972a8f4f1c8124e1cbc5
qkdflrgs <qkdflrgs@gmail.com>
2023-09-22 12:39:55 +0900

    feat/#105: API Endpoint 추가

--- fe/src/constants/endpoints.ts
+++ fe/src/constants/endpoints.ts
@@ -8,7 +8,10 @@ export const API_ENDPOINTS = {
     `/categories?includeImages=${includeImages}`,
 
   DELETE_PRODUCT: (productId: number) => `/products/${productId}`,
+  DELETE_PRODUCT_IMG: (imgId: number) => `/images/${imgId}`,
   POST_NEW_PRODUCT: () => '/products',
+  POST_PRODUCT_IMG: () => '/products/images',
+  PUT_PRODUCT: (productId: number) => `/products/${productId}`,
   PRODUCT_DETAIL: (productId: number) => `/products/${productId}`,
   PRODUCT_STATUS: (productId: number) => `/products/${productId}/status`,
   UPDATE_LIKE: (productId: number) => `/products/${productId}/likes`,
