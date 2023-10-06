468aa2f00d9f6b90c94097b5af3ad2ea91871661
qkdflrgs <qkdflrgs@gmail.com>
2023-10-06 13:15:09 +0900

    feat/#132: 채팅 리스트 페이지 구현

--- fe/src/pages/ChatPage.tsx
+++ fe/src/pages/ChatPage.tsx
@@ -1,7 +1,52 @@
-import { styled } from 'styled-components';
+import { useChatList } from '@api/chat/chat';
+import { ChatListItem } from '@components/ChatListItem/ChatListItem';
+import { Header } from '@components/Header/Header';
+import styled from 'styled-components';
+import { Loading } from './Loading';
+import { ErrorPage } from './ErrorPage';
+import { useAtom } from 'jotai';
+import { userInfoAtom } from '@atoms/userAtom';
 
 export const ChatPage = () => {
-  return <Page>채팅</Page>;
+  const [userInfo] = useAtom(userInfoAtom);
+  const { data, isLoading, isError } = useChatList(userInfo!.id);
+
+  if (isLoading) return <Loading />;
+  if (isError) return <ErrorPage />;
+
+  return (
+    <>
+      <Header>
+        <Header.Center>채팅</Header.Center>
+      </Header>
+      <Content>
+        {data.length === 0 && <EmptyItem>채팅 내역이 없습니다</EmptyItem>}
+        {data.map((item) => (
+          <ChatListItem key={item.id} item={item} />
+        ))}
+      </Content>
+    </>
+  );
 };
 
-const Page = styled.div``;
+const Content = styled.div`
+  padding: 56px 16px 56px 16px;
+  overflow-y: scroll;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const EmptyItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+`;
